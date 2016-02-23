bc90e47d15f9c822c52e8fad80163f16b453dc7a
Douglas H. Bradshaw <dhbradshaw@gmail.com>
2016-02-22 21:08:55 -0600

    Converts one of the 2^8 variants correctly.

--- dna_2_bytes/src/dna_2_bytes.rs
+++ dna_2_bytes/src/dna_2_bytes.rs
@@ -1,3 +1,6 @@
-pub fn to_bytes(s: &str) -> String {
-    s.to_string()
+pub fn to_bytes(s: &str) -> u8 {
+    match s {
+        "AAAA"=> 0,
+        _ => 1,
+    }
 }
--- dna_2_bytes/src/main.rs
+++ dna_2_bytes/src/main.rs
@@ -1,5 +1,5 @@
 extern crate dna2bytes;
 
 fn main() {
-	println!("Hello in dna_2_bytes: {}", dna2bytes::dna_2_bytes::to_bytes("GGACTTAC"));
+	println!("to_bytes: {}", dna2bytes::dna_2_bytes::to_bytes("AAAA"));
 }
